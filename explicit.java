c1292d6f32485862f885ff51d796835e24ca5aa0
vinaysingh9981 <vinay.singh998147@gmail.com>
2026-03-21 16:29:55 +0530

    explicit java data conversion

---
+++ explicit.java
@@ -0,0 +1,10 @@
+public class explicit {
+    public static void main(String[] args) {
+        int a= 10;
+        Object  o;
+        o=a;
+        int b=(int)o;
+        System.out.println(o+" "+b);
+
+    }
+}
