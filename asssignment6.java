f9f41f9796f6eb7b5c47fcc6eb7c2f9cff61282b
vinaysingh9981 <vinay.singh998147@gmail.com>
2026-03-24 23:16:04 +0530

    Add files via upload

---
+++ asssignment6.java
@@ -0,0 +1,20 @@
+package newproject;
+
+
+
+public class asssignment6 {
+	static int c = 0;
+	public static void main(String[] args) {
+		
+		int f ;
+		
+		
+		f= (9*c)/5+32;
+		
+		System.out.println("this is celsius to fahrenheit = "+f+" F");
+		
+		
+
+	}
+
+}
