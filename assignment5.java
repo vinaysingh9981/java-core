4ba9ef1ef61ece6972b39f0663bf58b1e9de60a1
vinaysingh9981 <vinay.singh998147@gmail.com>
2026-03-24 23:15:25 +0530

    Add files via upload
    variable

---
+++ assignment5.java
@@ -0,0 +1,19 @@
+package newproject;
+
+public class assignment5 {
+			int si,p,r,t;
+	public static void main(String[] args) {
+		
+		assignment5 obj= new assignment5();
+		 
+		obj.p=1000;
+		obj.r=3;
+		obj.t=3;
+		
+		obj.si=(obj.p*obj.r*obj.t)/100;
+		
+		System.out.println("this is simple interest SI= "+obj.si);
+
+	}
+
+}
